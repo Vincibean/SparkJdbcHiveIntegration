454691e59166c5f7a726e07783f97acf990b000a
Vincibean <andrebessi00@gmail.com>
2017-08-19 15:53:23 +0200

    Optimized Plane data representation

--- src/main/resources/db/migration/V1__Create_plane_table.sql
+++ src/main/resources/db/migration/V1__Create_plane_table.sql
@@ -1,11 +1,11 @@
 create table PLANES (
-    tailNum varchar(100) not null,
-    type varchar(100),
-    manufacturer varchar(100),
-    issueDate varchar(100),
-    model varchar(100),
-    status varchar(100),
-    aircraftType varchar(100),
-    engineType varchar(100),
-    constructionYear varchar(100)
+    tailNum varchar(7) not null,
+    type varchar(20),
+    manufacturer varchar(30),
+    issueDate varchar(10),
+    model varchar(20),
+    status varchar(20),
+    aircraftType varchar(30),
+    engineType varchar(15),
+    constructionYear varchar(4)
 );
